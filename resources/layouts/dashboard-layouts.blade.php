76ffc1d0f23d0b42f933cf57fcbb513ef99c3ff3
ferdi-alf <ferdialfianferdi.08@gmail.com>
2025-07-15 11:15:54 +0700

    first commit

---
+++ resources/layouts/dashboard-layouts.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @vite('resources/css/app.css')
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
+    <title>Dashboard</title>
+
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+    <script>
+        @if (session('alert'))
+            @php $alert = session('alert'); @endphp
+
+            Swal.fire({
+                icon: '{{ $alert['type'] }}',
+                title: '{{ $alert['title'] }}',
+                text: '{{ $alert['message'] }}',
+                confirmButtonColor: '#991b1b',
+                timer: 3000,
+                timerProgressBar: true
+            });
+        @endif
+
+        @if ($errors->any())
+            let errorMessages = '';
+            @foreach ($errors->all() as $error)
+                errorMessages += '• {{ $error }}\n';
+            @endforeach
+
+            Swal.fire({
+                icon: 'error',
+                title: 'Validation Error',
+                text: errorMessages,
+                confirmButtonColor: '#991b1b'
+            });
+        @endif
+    </script>
+</body>
+
+</html>
